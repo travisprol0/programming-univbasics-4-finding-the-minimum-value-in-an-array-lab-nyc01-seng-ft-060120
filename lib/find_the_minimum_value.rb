e6058a16bc6aeb2e012f35d7ca6b174dc72c6ef4
Travis Prol <travisjohnprol@gmail.com>
2020-05-06 22:23:47 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,7 +2,7 @@ def find_min_value(array)
   count = 0
   min_value = -1
   while count < array.length do
-    if min_value < array[count]
+    if min_value > array[count]
       min_value = array[count]
     end
     count += 1
